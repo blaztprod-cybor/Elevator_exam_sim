a9c00d99b3dc36d0b489fc2380404e834f6d3fbf
blaztprod-cybor <blaztprod@gmail.com>
2026-05-19 20:46:32 -0400

    Add Chrome PDF viewer link

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "elevator-exam-pwa-v15";
+const CACHE_VERSION = "elevator-exam-pwa-v16";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
@@ -11,7 +11,7 @@ const APP_SHELL_URLS = [
   "./review.html",
   "./results.html",
   "./viewer.html",
-  "./app.css?v=20260519-search-highlight2",
+  "./app.css?v=20260519-chrome-pdf-link",
   "./config.js?v=20260515-render-deploy",
   "./app.js?v=20260519-preview-search",
   "./pwa-register.js?v=20260519-install-page2",
